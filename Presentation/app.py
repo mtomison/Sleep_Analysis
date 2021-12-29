d7d82d30989fe9642e115c5d667025f6b0baef51
Laura Belnap <88058739+lbelnap20@users.noreply.github.com>
2021-12-29 03:39:29 -0600

    Adding html rough draft
    Adding to the correct branch

---
+++ Presentation/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+inputs = {
+    'timeInBed' :['text', 'Time in Bed:'],
+    'heartRate' :['text', 'Heart Rate:'],
+    'activity' :['text', 'Activity:'],
+    'stressfulDay' :['checkbox', 'Stressful Day?'],
+    'workedOut' :['checkbox', 'Worked Uut?'],
+    'drankCoffee' :['checkbox', 'Drank Coffee?'],
+    'drankTea' :['checkbox', 'Drank Tea?'],
+    'ateLate' :['checkbox', 'Ate Late?']
+
+}
